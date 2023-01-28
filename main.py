5d6f138b4217f4004856dfc2063cbbb34d9918f0
Varun <varunsiva360@gmail.com>
2023-01-28 14:46:11 -0600

    added varun greeting

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
 	name = input("What's your name: ")
 	if name == "satoshi":
 		print("hello satoshi!")
+	else if name == "varun":
+		print("sup varun!")
 	else:
 		print("goodbye " + name + "!")
 	
